d921a04d8a7ff153dba32073438f24dfeab0495d
ofir mossinson <ofirmoss@gmail.com>
2017-11-17 20:26:31 +0200

    "add contactUs and keywords"

--- homepage/js/gallery.js
+++ homepage/js/gallery.js
@@ -3,37 +3,36 @@
 var imgs = [{
         id: "0",
         url: "img/memes/0.jpg",
-        keywords: ["burning","smile","child"]
+        keywords: ["burning", "smile", "child"]
     },
     {
         id: "1",
         url: "img/memes/1.jpg",
-        keywords: ["crying","jorden","sad"]
+        keywords: ["crying", "jorden", "sad"]
     },
     {
         id: "2",
         url: "img/memes/2.jpg",
-        keywords: ["cartoon","blame","not"]
+        keywords: ["cartoon", "blame", "not"]
     },
     {
         id: "3",
         url: "img/memes/3.jpg",
-        keywords: ["one","man","simply"]
+        keywords: ["one", "man", "simply"]
     },
     {
         id: "4",
         url: "img/memes/4.jpg",
-        keywords: ["crazy","girl","smile"]
+        keywords: ["crazy", "girl", "smile"]
     }, {
         id: "5",
         url: "img/memes/5.jpg",
-        keywords: ["baby","success","victory"]
+        keywords: ["baby", "success", "victory"]
     }
 ];
 
 renderImgs(imgs);
 renderKeywords(imgs);
-
 
 function renderImgs(imgs) {
     var strHtml = '';
@@ -46,15 +45,52 @@ function renderImgs(imgs) {
     gallery.innerHTML = strHtml;
 }
 
-function renderKeywords(imgs){
+
+
+function renderKeywords(imgs) {
     var strHtml = '';
-    
-        imgs.forEach(function (img, idx) {
-            img.keywords.forEach(function (keyword, idx) {
-                strHtml += `<h3>${keyword} </h3>`
+    var keywordsRepeats = checkreapets(imgs);
+
+    keywordsRepeats.forEach(function (keyword) {
+        if (keyword.repeats === 1) strHtml += `<h6>${keyword.txt} </h6>`;
+        else if (keyword.repeats <= 10) strHtml += `<h5>${keyword.txt} </h5>`;
+        else if (keyword.repeats <= 20) strHtml += `<h4>${keyword.txt} </h4>`;
+        else if (keyword.repeats <= 30) strHtml += `<h3>${keyword.txt} </h3>`;
+        else if (keyword.repeats <= 40) strHtml += `<h2>${keyword.txt} </h2>`;
+        else if (keyword.repeats <= 50) strHtml += `<h1>${keyword.txt} </h1>`;
+    })
+
+    var keywords = document.querySelector('.keywords');
+    keywords.innerHTML = strHtml;
+}
+
+
+
+function checkreapets(imgs) {
+    var repeatscount = [{txt: imgs[0].keywords[0], repeats: 1}];
+    var counter = 1;
+
+    for (var i = 0; i < imgs.length; i++) {
+        for (var j = 0; j < imgs[i].keywords.length; j++) {
+            var currWord = imgs[i].keywords[j];
+            repeatscount.forEach(function (count) {
+                if (count.txt === currWord) counter++;
             })
+        if (counter === 1) {
+            repeatscount.push({
+                txt: currWord,
+                repeats: counter
             });
-    
-        var keywords = document.querySelector('.keywords');
-        keywords.innerHTML = strHtml;
+        }  repeatscount.forEach(function (count) {
+            if (count.txt === currWord) {
+                count.repeats= counter;
+            }
+        });
+        counter = 1;
+    }
+
+}
+
+repeatscount[0].repeats-=1;
+return repeatscount;
 }
